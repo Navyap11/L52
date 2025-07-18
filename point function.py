889cc36ab3581ab68326fa1010e311714323cfbf
navyap11 <ashwin.p.parmar@gmail.com>
2025-07-18 20:38:57 +0200

    f

---
+++ point function.py
@@ -0,0 +1,10 @@
+class pointeyy:
+    def __init__(self, x=0, y=0):
+        self.x=x
+        self.y=y
+
+    def __string__(self):
+        return "({0}, {1})".format(self.x, self.y)
+    
+position1= pointeyy(2,3)
+print(position1)
